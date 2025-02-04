4c51527477aaf7fe9157eadbcada587695f78a16
eisra <eisra3000ese@gmail.com>
2025-02-04 19:24:57 +0000

    0

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+/**
+ * Function that prints a message to STDOUT
+ * @param {string} message - The message to be printed
+ */
+function displayMessage(message) {
+    console.log(message);
+}
+
+module.exports = displayMessage;
