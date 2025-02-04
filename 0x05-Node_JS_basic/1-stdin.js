2d9faa5bea63e5f99d266ebd183afa45363f6fa2
eisra <eisra3000ese@gmail.com>
2025-02-04 19:45:28 +0000

    1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -8,9 +8,12 @@ process.stdout.write('Welcome to ALX, what is your name?\n');
 process.stdin.on('data', (data) => {
     const name = data.toString().trim();
     console.log(`Your name is: ${name}`);
-    process.exit();
+    
+    // Close the input stream before exiting
+    process.stdin.end();
 });
 
-process.on('exit', () => {
+// Listen for the input stream close event before exiting
+process.stdin.on('end', () => {
     console.log('This important software is now closing');
 });
