840ab48230c59460f0baef3b40faf859f5694340
eisra <eisra3000ese@gmail.com>
2025-02-05 17:25:15 +0000

    edit

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -3,17 +3,12 @@
  * A program that prompts the user for their name and displays a message.
  */
 
-process.stdout.write('Welcome to ALX, what is your name?\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
-    const name = data.toString().trim();
-    console.log(`Your name is: ${name}`);
-    
-    // Close the input stream before exiting
-    process.stdin.end();
+  process.stdout.write(`Your name is: ${data}`);
 });
 
-// Listen for the input stream close event before exiting
 process.stdin.on('end', () => {
-    console.log('This important software is now closing');
+  process.stdout.write('This important software is now closing\n');
 });
