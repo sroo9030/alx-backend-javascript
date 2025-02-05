b5bbf6bf9489829d6494d79caaf37528caddf329
eisra <eisra3000ese@gmail.com>
2025-02-05 17:28:31 +0000

    edit

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -3,8 +3,6 @@
  * Function that prints a message to STDOUT
  * @param {string} message - The message to be printed
  */
-function displayMessage(message) {
-    console.log(message);
-}
+const displayMessage = (message) => console.log(message);
 
 module.exports = displayMessage;
